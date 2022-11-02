f8363c3b7999258bb452e911227eeda719037b80
Thalita <108842621+Euthalita@users.noreply.github.com>
2022-11-02 16:23:41 -0300

    codigos

---
+++ script.js
@@ -0,0 +1,13 @@
+function mostrar(){
+    document.getElementById('aviso').style.display = 'block'
+}
+
+function ocultar(){
+    document.getElementById('aviso').style.display ='none'
+    document.getElementById('msg').style.display='none'
+}
+   
+
+function msg(){
+    document.getElementById('msg').style.display='block'
+}
