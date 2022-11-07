f7e72775708ae85f1907b85409a58801d19ce549
Thalita <108842621+Euthalita@users.noreply.github.com>
2022-11-07 16:14:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function mostrar(){
     document.getElementById('aviso').style.display = 'block'
+    document.getElementById('cel').style.display='none'
+    
 }
 
 function ocultar(){
     document.getElementById('aviso').style.display ='none'
     document.getElementById('msg').style.display='none'
+    document.getElementById('cel').style.display='block'
 }
    
 
 function msg(){
     document.getElementById('msg').style.display='block'
+    document.getElementById('cel').style.display='none'
 }
